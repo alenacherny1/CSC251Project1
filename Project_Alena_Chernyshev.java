42363b0e7637d21c7336057d624311b6b3cdaa37
alenacherny1 <159588004+alenacherny1@users.noreply.github.com>
2024-03-19 19:43:21 -0400

    Update Project_Alena_Chernyshev.java

--- Project_Alena_Chernyshev.java
+++ Project_Alena_Chernyshev.java
@@ -1,41 +1,42 @@
 import java.util.*;
+import java.util.Scanner;
 
 public class Project_Alena_Chernyshev
 {
    public static void main(String[] args)
    {
    //this method will display all of the info pulled from the policy class and display it
-   
-   Scanner key = new Scanner(System.in);
-   
-   System.out.print("Please enter the Policy Number: ");
+
+   File input = new File("PolicyInformation.txt");
+   Scanner key = new Scanner(input);
+   int smoker = 0, nonsmoker = 0;
+     
+     while(key.hasNext())
+       {
    int polNum = key.nextInt();
-   
-   key.nextLine();//clears buffer for string value after numeric
-   
-    System.out.print("\nPlease enter the Provider Name: ");
+   key.nextLine();//clears buffer for string value after numeric value
     String provName = key.nextLine();
-    
-    System.out.print("\nPlease enter the Policyholder's First Name: ");
     String firstName = key.nextLine();
-    
-    System.out.print("\nPlease enter the Policyholder's Last Name: ");
     String lastName = key.nextLine();
-    
-    System.out.print("\nPlease enter the Policyholder's Age: "); 
     int age = key.nextInt();
-    
     key.nextLine();
-    
-    System.out.print("\nPlease enter the Policyholder's Smoking Status (smoker/non-smoker): " );
     String smokingStatus = key.nextLine();
-    
-    System.out.print("\nPlease enter the Policyholder's Height (in inches): ");
     double height = key.nextDouble();
-    
-    System.out.print("\nPlease enter the Policyholder's Weight (in pounds) ");
     double weight = key.nextDouble();
-    
+
+      if(input.hasNext())  //checks for next line
+      input.nextLine(); //clears buffer
+      
+      if(input.hasNext())
+      input.nextLine();
+    if(smokingStatus.equals("smoker"));
+         {
+           smoker++;
+         }
+    else{
+      nonsmoker++;
+    }
+         
     Policy p = new Policy(polNum, provName, firstName, lastName, age, smokingStatus, height, weight);
     
     System.out.println("Policy Number: " + p.GetPolNum());
@@ -48,5 +49,8 @@ public class Project_Alena_Chernyshev
     System.out.printf("\nPolicyholder's Weight %,.2f pounds", p.GetWeight());
     System.out.printf("\nPolicyholder's BMI: %.2f", p.BMI());
     System.out.printf("\nPolicy Price: $%,.2f ", p.Price());
-    }
+      }
+     System.out.printf("\nThe number of policies with a smoker is: %d", smoker);
+     System.out.printf("\nThe number of policies with a smoker is: %d", nonsmoker);
+   }
  }
